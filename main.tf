7aaf4fd92b282d8ddafd90ea39fccbb1f89e273f
Taras Rudenko <rudenko.taras@gmail.com>
2021-09-02 18:01:13 +0300

    Binary log disabled in postgres instance.

--- main.tf
+++ main.tf
@@ -221,7 +221,6 @@ resource "google_sql_database_instance" "postgres" {
     }
    
     backup_configuration {
-      binary_log_enabled = true
       enabled = true
       start_time = "00:00"
     }
