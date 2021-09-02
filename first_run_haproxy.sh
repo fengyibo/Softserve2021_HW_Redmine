9f5b026efc40bb41003005d33264bfed2c28571a
technow0rker <technow0rker@gmail.com>
2021-09-03 00:34:21 +0300

    Added HTTP basic auth for stats access; adjusted configuration template file to reflect auth settings

--- first_run_haproxy.sh
+++ first_run_haproxy.sh
@@ -3,19 +3,24 @@
 # This script installs 'haproxy' package
 # and sets it to run at boot
 
+# Config file location
+CFG_FILE="/etc/haproxy/haproxy.cfg"
+# Config file desired permissions
+CFG_PERMS=400
+
 echo -e "\nInstalling HAProxy"
 
 if ! [ -x /usr/bin/sudo ]; then
-    echo -e "\nPlease install 'sudo' package first. Exiting."
+    echo -e "\nPlease install 'sudo' package first. Exiting.\n"
     exit 1
 fi
 
 
-echo "Updating packages repositories"
+echo -e "Updating packages repositories\n"
 /usr/bin/sudo /usr/bin/apt-get update
 
 
-echo "Installing HAProxy package"
+echo -e "Installing HAProxy package\n"
 /usr/bin/sudo /usr/bin/apt-get install -y haproxy
 
 if [ $? -ne 0 ]; then
@@ -24,10 +29,17 @@ if [ $? -ne 0 ]; then
 fi
 
 echo -e "\nPackage successfully installed."
-echo -e "Configuration file is located in /etc/haproxy/haproxy.cfg"
+echo "Configuration file: ${CFG_FILE}"
 echo -e "\nEnabling service at startup"
 
 /usr/bin/sudo /usr/bin/systemctl enable haproxy
+
+echo "Fixing config file permissions (${CFG_PERMS})"
+/usr/bin/sudo /usr/bin/chmod ${CFG_PERMS} ${CFG_FILE}
+
+if [ $? -ne 0 ]; then
+    echo -e "WARNING: Error while fixing permissions. Please fix them manually to ${CFG_PERMS}"
+fi
 
 echo -e "\nDONE."
 
